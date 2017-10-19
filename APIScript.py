4c7410eb7d2b3933398ecbf4471a71b622a99697
tellurium4 <tellurium4@gmail.com>
2017-10-19 18:57:50 -0400

    Simple API Script

---
+++ APIScript.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import requests
+import subprocess
+# 129 for 6.45M entries
+# limit = 50000 #(the max)
+print("STARTING API DOWNLOAD WITH PYTHON")
+file = open("CrimeTotal.json","w")
+for x in range(0,2):
+	offset = 5*x
+	limit = 5
+	recount = 0
+	flag = True
+	response = requests.get("https://data.cityofchicago.org/resource/6zsd-86xi.json?$limit=%s&$offset=%s&$order=date&$$app_token=CwFNFRUwnqGC8LqCmVWONgGE8" % (limit,offset))
+	status = response.status_code 
+	while flag:
+		if status != 200:
+			recount += 1
+			if recount > 10:
+				flag = False
+				print("Failed to get record\noffset:%s" % (offset))
+			else:
+				response = requests.get("https://data.cityofchicago.org/resource/6zsd-86xi.json?$limit=%s&$offset=%s&$order=date&$$app_token=CwFNFRUwnqGC8LqCmVWONgGE8" % (limit,offset))
+		else:
+			#record data
+			file.write(response.content)
+			flag = False # just in case
+			break
+
+
+	# print(response.content)
+file.close()
+print("COMPLETED API DOWNLOAD WITH PYTHON")
+bashCommand = "hadoop fs -put CrimeTotal.json /tmp/CrimeTotal.json"
+print("STARTING HADOOP DISTRIUBUTED FILE SYSTEM TRANSFER USING %s" % bashCommand)
+process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+output, error = process.communicate()
