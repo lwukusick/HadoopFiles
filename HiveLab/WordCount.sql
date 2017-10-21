14a27c292d663ced610407d7b4c0fa836ce79d12
Luke Wukusick <lwukusick@gmail.com>
2017-10-21 16:16:16 -0400

    Added Task 2 files

---
+++ HiveLab/WordCount.sql
@@ -0,0 +1,15 @@
+ADD JAR ./StripUpper.jar;
+CREATE temporary function Strip as 'edu.rosehulman.wukusil.Split';
+CREATE temporary function Upper as 'edu.rosehulman.wukusil.Upper';
+
+CREATE DATABASE IF NOT EXISTS ${hiveconf:databaseName};
+USE ${hiveconf:databaseName};
+CREATE EXTERNAL TABLE IF NOT EXISTS ${hiveconf:tableName}
+(line String)
+ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t'
+STORED AS TEXTFILE
+LOCATION '${hiveconf:inputLocation}';
+
+SELECT word, COUNT(*)
+FROM ${hiveconf:tableName} LATERAL VIEW explode(Strip(Upper(split(line," ")))) flatTable AS word
+GROUP BY word;
