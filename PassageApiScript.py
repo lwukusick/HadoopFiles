f7d6346697bd13914b7f61afd4f7c3422858112e
root <root@hadoop-m1.c.hadoop-project-180619.internal>
2017-10-22 18:49:43 +0000

    Finished Safe Passage data download script

--- PassageApiScript.py
+++ PassageApiScript.py
@@ -7,32 +7,15 @@ import subprocess
 def removeArray(content):
 	return content[1:-2].replace("\n,", "\n")
 
-# def getLastOffset():
-# 	try:
-# 		file = open("OffsetFile")
-# 	except:
-# 		file = open("file","w")
-# 		file.write("0")
-# 	else:
-# 		file = open("OffsetFile")
-# 	return file.readline()
+years = {"1314":"nm24-d5en", "1415":"t8dp-yzqg", "1516":"kvm8-tw23", "1617":"65ce-agii"}
 
-# initialOffset = getLastOffset()
-# file.close()
-limit = 1000 #(the max)
-offset_K = limit
-whileloopflag = True
-x = 0
-print("STARTING API DOWNLOAD WITH PYTHON")
+for key, value in years.iteritems():
+	print("STARTING API DOWNLOAD WITH PYTHON FOR SCHOOL YEAR %s" % key)
 
-file = open("CrimeTotal.json","w")
-# while whileloopflag:
-for x in range(0,2):
-	offset = offset_K*x
-	x = x + 1
+	file = open("Passage.json","w")
 	recount = 0
 	flag = True
-	response = requests.get("https://data.cityofchicago.org/resource/6zsd-86xi.json?$limit=%s&$offset=%s&$order=date&$$app_token=CwFNFRUwnqGC8LqCmVWONgGE8" % (limit,offset))
+	response = requests.get("https://data.cityofchicago.org/resource/%s.json" % value)
 	status = response.status_code 
 	while flag:
 		if status != 200:
@@ -41,20 +24,29 @@ for x in range(0,2):
 				flag = False
 				print("Failed to get record %s\noffset:%s" % (x,offset))
 			else:
-				response = requests.get("https://data.cityofchicago.org/resource/6zsd-86xi.json?$limit=%s&$offset=%s&$order=date&$$app_token=CwFNFRUwnqGC8LqCmVWONgGE8" % (limit,offset))
+				response = requests.get("https://data.cityofchicago.org/resource/%s.json" % value)
 		else:
 			#record data
-			if len(response.content) == 3:
-				whileloopflag = False
 			cleaned_data = removeArray(response.text)
 			file.write("%s\n" % cleaned_data)
 			flag = False # just in case
 			break
 
-	# print(response.content)
-file.close()
-print("COMPLETED API DOWNLOAD WITH PYTHON")
-bashCommand = "hadoop fs -put -f CrimeTotal.json /tmp/data/crime/CrimeTotal.json"
-print("STARTING HADOOP DISTRIUBUTED FILE SYSTEM TRANSFER USING: %s" % bashCommand)
-process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-output, error = process.communicate()
+	file.close()
+	print("COMPLETED API DOWNLOAD WITH PYTHON")
+	
+	bashCommand = "hadoop fs -mkdir -p /tmp/data/passage/year=%s" % key
+	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+	output, error = process.communicate()
+
+	bashCommand = "hadoop fs -put -f Passage.json /tmp/data/passage/year=%s/" % key
+	print("STARTING HADOOP DISTRIUBUTED FILE SYSTEM TRANSFER USING: %s" % bashCommand)
+	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+	output, error = process.communicate()
+	print(error)
+
+	#bashCommand = "rm Passage.json"
+	#print("CLEANING UP %s" % key)
+	#process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+	#output, error = process.communicate()
+
