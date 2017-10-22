2013b13db6246181c23c2edf6e7cd6dfdfaeb698
root <root@hadoop-m1.c.hadoop-project-180619.internal>
2017-10-22 16:55:36 +0000

    Changed APIScript output directory and changed crimeData fields

--- Project/importCrimeJson.sql
+++ Project/importCrimeJson.sql
@@ -2,10 +2,10 @@ CREATE EXTERNAL TABLE crimeData
 (
 latitude string,
 longitude string,
-description string--,
---arrest boolean,
---domestic boolean,
---fbi_code string
+description string,
+arrest boolean,
+domestic boolean,
+fbi_code string
 )
 ROW FORMAT SERDE 'org.apache.hive.hcatalog.data.JsonSerDe'
 STORED AS TEXTFILE
