22f90ed260a2fe85eff0659483417c962eb0a5e8
root <root@hadoop-m1.c.hadoop-project-180619.internal>
2017-10-22 19:16:43 +0000

    Updated scripts to work with new schema/pig output

--- Project/withinRangeLine.sql
+++ Project/withinRangeLine.sql
@@ -1,9 +1,8 @@
 --Useage: have -hiveconf LINESTRING='x y, [x y]*' DIST='<num>' -f withinRangeLine.sql
-add jar ./esri-geometry-api.jar ./spatial-sdk-hadoop.jar ./StringToDouble.jar;
+add jar ./esri-geometry-api.jar ./spatial-sdk-hadoop.jar;
 
-create temporary function StoD as 'StringToDouble';
 create temporary function ST_Point as 'com.esri.hadoop.hive.ST_Point';
 create temporary function ST_Distance as 'com.esri.hadoop.hive.ST_Distance';
 create temporary function ST_LineString as 'com.esri.hadoop.hive.ST_LineString';
 
-select * from crimeData where latitude is not null and ST_Distance(ST_Point(StoD(latitude), StoD(longitude)), ST_LineString('linestring (${hiveconf:linestring})')) <= ${hiveconf:dist};
+select * from crimeData where latitude is not null and ST_Distance(ST_Point(latitude, longitude), ST_LineString('linestring (${hiveconf:linestring})')) <= ${hiveconf:dist};
