752f276af920ff173880fc87e8877afc33351e7d
root <root@hadoop-m1.c.hadoop-project-180619.internal>
2017-10-29 23:26:24 +0000

    Added split-by

--- Lab7/Task1/task1Wukusil.sh
+++ Lab7/Task1/task1Wukusil.sh
@@ -2,17 +2,17 @@ sqoop import --connect jdbc:mysql://$1/sqooptest --username root --table Employe
 
 sqoop import --connect jdbc:mysql://$1/sqooptest --username root --table Employees -m 1 --target-dir /tmp/sqoopMapOutput
 
-sqoop import --connect jdbc:mysql://$1/sqooptest --username root --table Employees -m 2 --target-dir /tmp/sqoopSeqOutput --as-sequencefile
+sqoop import --connect jdbc:mysql://$1/sqooptest --username root --table Employees -m 2 --split-by eid --target-dir /tmp/sqoopSeqOutput --as-sequencefile
 
-sqoop import --connect jdbc:mysql://$1/sqooptest --username root --table Employees -m 2 --warehouse-dir /tmp/sqoop
+sqoop import --connect jdbc:mysql://$1/sqooptest --username root --table Employees -m 2 --split-by eid --warehouse-dir /tmp/sqoop
 
-sqoop import --connect jdbc:mysql://$1/sqooptest --username root --table Employees -m 2 --warehouse-dir /tmp/sqoop --null-string "This is a Null String" --fields-terminated-by '\t'
+sqoop import --connect jdbc:mysql://$1/sqooptest --username root --table Employees -m 2 --split-by eid --warehouse-dir /tmp/sqoop --null-string "This is a Null String" --fields-terminated-by '\t'
 
 hive -e "create database if not exists sqooptest"
 
-sqoop import --connect jdbc:mysql://$1/sqooptest --username root -m 2 --table Employees --hive-import --create-hive-table --hive-table sqooptest.Employees 
+sqoop import --connect jdbc:mysql://$1/sqooptest --username root -m 2 --split-by eid --table Employees --hive-import --create-hive-table --hive-table sqooptest.Employees 
 
-sqoop import --connect jdbc:mysql://$1/sqooptest --username root -m 2 --table Employees --hive-import --create-hive-table --hive-table sqooptest.Employees --null-string "This is a Null String" --fields-terminated-by '\t'
+sqoop import --connect jdbc:mysql://$1/sqooptest --username root -m 2 --split-by eid --table Employees --hive-import --create-hive-table --hive-table sqooptest.Employees --null-string "This is a Null String" --fields-terminated-by '\t'
 
 sqoop import-all-tables --connect jdbc:mysql://$1/sqooptest --username root -m 1 --warehouse-dir /tmp/sqoopAll
 
