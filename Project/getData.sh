c334acbef62c451ebc73f2e9ed14cb1aa1b90d47
root <root@hadoop-m1.c.hadoop-project-180619.internal>
2017-11-03 17:21:26 +0000

    Moved files around and updated withinRange hive script

--- Project/getData.sh
+++ Project/getData.sh
@@ -6,7 +6,7 @@ else
   echo "Crime import failed! Moving on to safe passage data"
 fi
 
-if python PassageApiScript.py ; then
+if python PassageApiScript.py && pig -p output=/tmp/data/passage_cleaned/year=1516 -p input=/tmp/data/passage/year=1516 cleanPassageData.pig && pig -p output=/tmp/data/passage_cleaned/year=1617 -p input=/tmp/data/passage/year=1617 cleanPassageData.pig ; then
   echo "Imported passage data"
 else 
   echo "Passage import failed!"
--- Project/withinRangeLine.sql
+++ Project/withinRangeLine.sql
@@ -1,8 +1,8 @@
 --Useage: have -hiveconf LINESTRING='x y, [x y]*' DIST='<num>' -f withinRangeLine.sql
-add jar ./esri-geometry-api.jar ./spatial-sdk-hadoop.jar;
+add jar ./esri-geometry-api.jar ./spatial-sdk-hadoop.jar ./DistanceLine.jar;
 
 create temporary function ST_Point as 'com.esri.hadoop.hive.ST_Point';
 create temporary function ST_Distance as 'com.esri.hadoop.hive.ST_Distance';
 create temporary function ST_LineString as 'com.esri.hadoop.hive.ST_LineString';
 
-select * from crimeData where latitude is not null and ST_Distance(ST_Point(latitude, longitude), ST_LineString('linestring (${hiveconf:linestring})')) <= ${hiveconf:dist};
+select * from crimeData where latitude is not null and ST_GeodesicLengthWGS84(DistanceLine(ST_LineString('linestring (${hiveconf:linestring})'), ST_Point(latitude, longitude))) <= ${hiveconf:dist};
