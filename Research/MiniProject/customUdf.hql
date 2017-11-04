054a1bc68d379eaee763f358d00f5fc46a03ce68
Luke Wukusick <lwukusick@gmail.com>
2017-11-03 23:47:45 -0400

    Stuff

--- Research/MiniProject/customUdf.hql
+++ Research/MiniProject/customUdf.hql
@@ -2,7 +2,7 @@ add jar esri-geometry-api.jar spatial-sdk-hadoop.jar DistanceLine.jar;
 create temporary function ST_Point as 'com.esri.hadoop.hive.ST_Point';
 create temporary function ST_GeodesicLengthWGS84 as 'com.esri.hadoop.hive.ST_GeodesicLengthWGS84';
 create temporary function ST_SetSRID as 'com.esri.hadoop.hive.ST_SetSRID';
-create temporary function ST_DistanceLine as 'ST_DistanceLine';
+create temporary function ST_DistanceLine as 'edu.rosehulman.wukusil.ST_DistanceLine';
 create temporary function ST_LineString as 'com.esri.hadoop.hive.ST_LineString';
 
 select name, ST_GeodesicLengthWGS84(ST_SetSRID(ST_DistanceLine(ST_LineString(multilinestring), ST_Point(long, lat)), 4326)) from customUdfActivity;
--- Research/Task2/ST_ManhattanDistance.java
+++ Research/Task2/ST_ManhattanDistance.java
@@ -1,11 +1,7 @@
+package edu.rosehulman.wukusil;
 import com.esri.hadoop.hive.GeometryUtils;
 import com.esri.hadoop.hive.ST_GeometryAccessor;
-import com.esri.core.geometry.Operator.Type;
-import com.esri.core.geometry.OperatorFactoryLocal;
-import com.esri.core.geometry.OperatorProximity2D;
 import com.esri.core.geometry.Point;
-import com.esri.core.geometry.Polyline;
-import com.esri.core.geometry.Proximity2DResult;
 import com.esri.core.geometry.ogc.OGCGeometry;
 
 import org.apache.hadoop.io.BytesWritable;
@@ -14,7 +10,6 @@ import org.apache.hadoop.io.DoubleWritable;
 
 public class ST_ManhattanDistance extends ST_GeometryAccessor {
 	final BytesWritable resultDouble = new BytesWritable();
-	OperatorProximity2D prox = (OperatorProximity2D)OperatorFactoryLocal.getInstance().getOperator(Type.Proximity2D);
 
     public DoubleWritable evaluate(BytesWritable geometryref1, BytesWritable geometryref2) {
 		if (geometryref1 == null || geometryref2 == null) {
@@ -24,7 +19,7 @@ public class ST_ManhattanDistance extends ST_GeometryAccessor {
 		OGCGeometry ogcGeom1 = GeometryUtils.geometryFromEsriShape(geometryref1);
 		OGCGeometry ogcGeom2 = GeometryUtils.geometryFromEsriShape(geometryref2);
 		
-		Point startPoint = (Point)ogcGeom2.getEsriGeometry();
+		Point startPoint = (Point)ogcGeom1.getEsriGeometry();
 		Point endPoint = (Point)ogcGeom2.getEsriGeometry();
 		
 		return new DoubleWritable(Math.abs(startPoint.getX() - endPoint.getX()) + Math.abs(startPoint.getY() - endPoint.getY()));
