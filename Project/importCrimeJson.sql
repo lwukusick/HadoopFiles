4f0f151550b882135bf65a04c18204ca4ae7eac3
tellurium4 <tellurium4@gmail.com>
2017-11-08 16:55:58 -0500

    added partitioning support

--- Project/importCrimeJson.sql
+++ Project/importCrimeJson.sql
@@ -15,3 +15,23 @@ domestic boolean
 ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
 STORED AS TEXTFILE
 LOCATION  '/tmp/data/crime/cleaned/';
+
+CREATE TABLE IF NOT EXISTS yearCrimeData
+(
+hour int,
+month int,
+crimeDate string,
+latitude double,
+longitude double,
+primaryType string,
+description string,
+fbi_code string,
+arrest boolean,
+domestic boolean
+)
+PARTITIONED BY (year int)
+ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
+STORED AS ORC;
+
+Set hive.exec.dynamic.partition.mode=nonstrict;
+INSERT INTO TABLE yearCrimeData partition(year) select hour, month, crimeData, latitude, longitude, primaryType, description, fbi_code, arrest, domestic from crimeDataSample;
